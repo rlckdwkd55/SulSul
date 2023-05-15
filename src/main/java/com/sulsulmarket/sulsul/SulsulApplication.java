d179b6209a028d223687b180ebca1d68f8b8021a
gichang <rlckdwkd55@naver.com>
2023-05-15 22:41:52 +0900

    FirstCommit

--- src/main/java/com/sulsulmarket/sulsul/SulsulApplication.java
+++ src/main/java/com/sulsulmarket/sulsul/SulsulApplication.java
@@ -3,6 +3,7 @@ package com.sulsulmarket.sulsul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 서버실행
 @SpringBootApplication
 public class SulsulApplication {
 
