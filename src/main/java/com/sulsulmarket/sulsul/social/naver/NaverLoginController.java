4cae1e28c5b1cefd1c44c8bc9acfeb551b57f13e
jimin0601 <jimin600155@naver.com>
2023-06-10 14:01:28 +0900

    NAVER API TEST SUCCESS 2023-06-10 JM

--- src/main/java/com/sulsulmarket/sulsul/social/naver/NaverLoginController.java
+++ src/main/java/com/sulsulmarket/sulsul/social/naver/NaverLoginController.java
@@ -1,41 +1,49 @@
-//package com.sulsulmarket.sulsul.social.naver;
-//
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//
-//@RestController
-//@Slf4j
-//public class NaverLoginController {
-//
-//    private final NaverLoginService naverLoginService;
-//
-//    @Autowired
-//    public NaverLoginController(NaverLoginService naverLoginService) {
-//        this.naverLoginService = naverLoginService;
-//    }
-//
-//    @GetMapping("/api/login/naver")
-//    public String naverLogin() {
-//        String authorizationUrl = naverLoginService.getAuthorizationUrl();
-//        log.info("[INFO] authorizationUrl -> {}", authorizationUrl);
-//        return "redirect:" + authorizationUrl;
-//    }
-//
-//    @PostMapping("/api/login/naver/callback")
-//    public ResponseEntity<String> naverLoginCallback(@RequestParam String code) {
-//        String tokenResponse = naverLoginService.getTokenResponse(code);
-//        return ResponseEntity.ok(tokenResponse);
-//    }
-//
-//    @GetMapping("/api/userInfo")
-//    public ResponseEntity<NaverUserDTO> getUserInfo(@RequestParam String code) {
-//        NaverUserDTO userInfo = naverLoginService.getUserInfo(code);
-//        return ResponseEntity.ok(userInfo);
-//    }
-//}
+package com.sulsulmarket.sulsul.social.naver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@Slf4j
+public class NaverLoginController {
+
+    private final NaverLoginService naverLoginService;
+
+    @Autowired
+    public NaverLoginController(NaverLoginService naverLoginService) {
+        this.naverLoginService = naverLoginService;
+    }
+
+    /**
+     * Naver Login redirect url 로그인 페이지 이동 후 인증 요청 Controller
+     */
+    @GetMapping("/api/login/naver")
+    public String naverLogin() {
+
+        String authorizationUrl = naverLoginService.getAuthorizationUrl();
+        log.info("[INFO] authorizationUrl -> {}", authorizationUrl);
+        return "redirect:" + authorizationUrl;
+    }
+
+    /**
+     * 위에서 받아온 authorization_code parameter 받아 createToken 발급 바독 해당 Token 가지고 User Info Get
+     */
+    @PostMapping("/api/oauth2/naver/callback")
+    public ResponseEntity<Object> naverLoginCallback(@RequestParam String code) {
+
+        try {
+            NaverUserDTO userDTO = naverLoginService.getUserInfoByAccessToken(code);
+            return ResponseEntity.status(HttpStatus.OK).body(userDTO);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+
+    }
+}
--- src/main/java/com/sulsulmarket/sulsul/social/naver/NaverLoginService.java
+++ src/main/java/com/sulsulmarket/sulsul/social/naver/NaverLoginService.java
@@ -1,91 +1,98 @@
-//package com.sulsulmarket.sulsul.social.naver;
-//
-//import com.fasterxml.jackson.databind.JsonNode;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.http.*;
-//import org.springframework.stereotype.Service;
-//import org.springframework.util.LinkedMultiValueMap;
-//import org.springframework.util.MultiValueMap;
-//import org.springframework.web.client.RestTemplate;
-//
-//import java.io.IOException;
-//
-//@Service
-//@Slf4j
-//public class NaverLoginService {
-//
-//    @Value("${spring.security.oauth2.client.registration.naver.client-id}")
-//    private String clientId;
-//
-//    @Value("${spring.security.oauth2.client.registration.naver.client-secret}")
-//    private String clientSecret;
-//
-//    @Value("${spring.security.oauth2.client.registration.naver.redirect-uri}")
-//    private String redirectUrl;
-//
-//    public String getAuthorizationUrl() {
-//        return "https://nid.naver.com/oauth2.0/authorize?client_id=" + clientId + "&redirect_uri=" + redirectUrl + "&response_type=code";
-//    }
-//
-//    public String getTokenResponse(String code) {
-//        String tokenUrl = "https://nid.naver.com/oauth2.0/token";
-//        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-//        params.add("client_id", clientId);
-//        params.add("client_secret", clientSecret);
-//        params.add("redirect_uri", redirectUrl);
-//        params.add("code", code);
-//        params.add("grant_type", "authorization_code");
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-//
-//        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(params, headers);
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        ResponseEntity<String> response = restTemplate.postForEntity(tokenUrl, requestEntity, String.class);
-//        String tokenResponse = response.getBody();
-//        log.info("[INFO] tokenResponse -> {}", tokenResponse);
-//        return tokenResponse;
-//    }
-//
-//    public NaverUserDTO getUserInfo(String code) {
-//        String tokenResponse = getTokenResponse(code);
-//        log.info("tokenResponse : -> {}", tokenResponse);
-//
-//        // 토큰 응답을 파싱하여 필요한 정보 추출
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        try {
-//            JsonNode jsonNode = objectMapper.readTree(tokenResponse);
-//            String accessToken = jsonNode.get("access_token").asText();
-//            log.info("accessToken -> {}", accessToken);
-//
-//            // 토큰을 사용하여 회원 정보를 가져오는 API 호출
-//            String userInfoUrl = "https://openapi.naver.com/v1/nid/me";
-//            HttpHeaders userInfoHeaders = new HttpHeaders();
-//            userInfoHeaders.set("Authorization", "Bearer " + accessToken);
-//            HttpEntity<String> userInfoRequestEntity = new HttpEntity<>(userInfoHeaders);
-//            RestTemplate restTemplate = new RestTemplate();
-//            ResponseEntity<String> userInfoResponse = restTemplate.exchange(userInfoUrl, HttpMethod.GET, userInfoRequestEntity, String.class);
-//            log.info("userInfoResponse -> {}", userInfoResponse);
-//            String userInfo = null;
-//            if (userInfoResponse.getStatusCode() == HttpStatus.PERMANENT_REDIRECT) {
-//                String redirectUrl = userInfoResponse.getHeaders().getLocation().toString();
-//                ResponseEntity<String> redirectResponse = restTemplate.exchange(redirectUrl, HttpMethod.GET, userInfoRequestEntity, String.class);
-//                userInfo = redirectResponse.getBody();
-//            } else {
-//                userInfo = userInfoResponse.getBody();
-//            }
-//
-//            log.info("[INFO] userInfo -> {}", userInfo);
-//
-//            // 회원 정보를 파싱하여 NaverUserDTO 객체로 반환
-//            NaverUserDTO userInfoObj = objectMapper.readValue(userInfo, NaverUserDTO.class);
-//            return userInfoObj;
-//        } catch (IOException e) {
-//            log.error("[ERROR] Failed to parse token response or user info", e);
-//            throw new IllegalStateException("Failed to parse token response or user info", e);
-//        }
-//    }
-//}
+package com.sulsulmarket.sulsul.social.naver;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+@Service
+@Slf4j
+public class NaverLoginService {
+
+    @Value("${spring.security.oauth2.client.registration.naver.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.naver.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.naver.redirect-uri}")
+    private String redirectUrl;
+
+    /**
+     * naver login 인증 요청하는 메서드 return by client_id, redirect-url
+     */
+    public String getAuthorizationUrl() {
+        return "https://nid.naver.com/oauth2.0/authorize?client_id=" + clientId + "&redirect_uri=" + redirectUrl + "&response_type=code";
+    }
+
+    /**
+     * 위에서 인증 후 받아온 code + 필수 파라미터를 넘기고 Create Token 가지고 userInfo Get
+     */
+    public NaverUserDTO getUserInfoByAccessToken(String code) {
+        String tokenUrl = "https://nid.naver.com/oauth2.0/token";
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("client_id", clientId);
+        params.add("client_secret", clientSecret);
+        params.add("redirect_uri", redirectUrl);
+        params.add("code", code);
+        params.add("grant_type", "authorization_code");
+        log.info("[INFO] Create Token Param -> client_id {}, client_secret {}, redirect_uri {}, code", clientId, clientSecret, redirectUrl, code);
+
+        HttpHeaders tokenHeaders = new HttpHeaders();
+        // Header 세팅 "application/x-www-form-urlencoded" type
+        tokenHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        // HttpEntity 생성 위에 params, header 담음
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(params, tokenHeaders);
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+
+            // restTemplate -> post(request url, entity(body, header), return class)
+            ResponseEntity<String> response = restTemplate.postForEntity(tokenUrl, requestEntity, String.class);
+            // response.getBody 메서드를 통해 token 정보를 가져옴
+            String accessToken = response.getBody();
+            log.info("[INFO] accessToken -> {}", accessToken);
+
+            // Json parsing 을 위한 ObjetMapper
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            try {
+                // Json 객체의 특정 필드 값을 가져옴
+                JsonNode jsonNode = objectMapper.readTree(accessToken);
+                // JsonNode access_token By String type return
+                String getToken = jsonNode.get("access_token").asText();
+
+                // 토큰을 사용하여 회원 정보를 가져오는 API Call
+                String getUserInfoUrl = "https://openapi.naver.com/v1/nid/me";
+                HttpHeaders userInfoHeader = new HttpHeaders();
+                userInfoHeader.set("Authorization", "Bearer " + getToken);
+
+
+                HttpEntity<String> userInfoEntity = new HttpEntity<>(userInfoHeader);
+                ResponseEntity<String> responseEntity = restTemplate.exchange(getUserInfoUrl, HttpMethod.GET, userInfoEntity, String.class);
+
+                String userInfo = responseEntity.getBody();
+                log.info("[INFO] userInfo -> {}", userInfo);
+
+                NaverUserDTO userDTO = objectMapper.readValue(userInfo, NaverUserDTO.class);
+                return userDTO;
+
+            } catch (IOException e) {
+                log.error("[ERROR] Failed to parse token response or user info", e);
+                throw new IllegalStateException("Failed to parse token response or user info");
+            }
+        } catch (HttpClientErrorException e) {
+            log.error("[ERROR] Failed to get user info from Naver API", e);
+            throw new IllegalStateException("Failed to get user info from Naver API", e);
+        }
+    }
+}
