b33da365de403207a10dd9f6c9de61b2f537f9fa
jimin0601 <jimin600155@naver.com>
2023-07-16 21:09:19 +0900

    Review Reply api add 2023-07-16 JM

--- src/main/java/com/sulsulmarket/sulsul/reviewReply/dao/ReviewReplyDao.java
+++ src/main/java/com/sulsulmarket/sulsul/reviewReply/dao/ReviewReplyDao.java
@@ -5,6 +5,8 @@ import com.sulsulmarket.sulsul.reviewReply.mapper.ReviewReplyMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class ReviewReplyDao implements ReviewReplyMapper {
 
@@ -14,5 +16,10 @@ public class ReviewReplyDao implements ReviewReplyMapper {
     @Override
     public int reviewReplyAdd(ReviewReplyDTO reviewReplyDTO) {
         return reviewReplyMapper.reviewReplyAdd(reviewReplyDTO);
+    }
+
+    @Override
+    public List<ReviewReplyDTO> getReviewReply(int reviewNo) {
+        return reviewReplyMapper.getReviewReply(reviewNo);
     }
 }
--- src/main/java/com/sulsulmarket/sulsul/reviewReply/service/ReviewReplyService.java
+++ src/main/java/com/sulsulmarket/sulsul/reviewReply/service/ReviewReplyService.java
@@ -1,5 +1,6 @@
 package com.sulsulmarket.sulsul.reviewReply.service;
 
+import com.sulsulmarket.sulsul.Util.SulSulUil;
 import com.sulsulmarket.sulsul.member.dao.MemberDao;
 import com.sulsulmarket.sulsul.member.dto.MemberDTO;
 import com.sulsulmarket.sulsul.review.dao.ReviewDao;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -49,6 +51,36 @@ public class ReviewReplyService {
             throw new NullPointerException("답글을 적을 리뷰 글을 찾을 수가 없습니다.");
         }
 
-        //TODO 리뷰 답글 작성 필요
+        if(reviewReplyDTO.getREPLY_CONTENT() == null || reviewReplyDTO.getREPLY_CONTENT().length() == 0 || reviewReplyDTO.getREPLY_CONTENT().equals("")) {
+            log.warn("Review Reply Contents Is Null ! ! !");
+            throw new IllegalArgumentException("리뷰 내용이 없습니다.");
+        }
+
+        try {
+            reviewReplyDTO.setREPLY_ID(Math.toIntExact(SulSulUil.getNextSequence()));
+            log.info("ReviewReplyDTO ===>>> [{}]", reviewReplyDTO.toString());
+            reviewReplyDao.reviewReplyAdd(reviewReplyDTO);
+            log.info("Review Reply Insert Is Success ! ! !");
+        } catch (Exception e) {
+            log.error("Review Reply Insert Exception : {}", e);
+        }
+    }
+
+    public List<ReviewReplyDTO> getReviewReplyListByReviewNo(int reviewNo) {
+
+        if(reviewNo <= 0) {
+            log.error("ReviewNo [{}] < 0 Illegal Review No ! ! !");
+            throw new IllegalArgumentException("잘못된 리뷰 번호입니다.");
+        }
+
+        List<ReviewReplyDTO> reviewReplyList = reviewReplyDao.getReviewReply(reviewNo);
+
+        if (reviewReplyList == null || Objects.isNull(reviewReplyList)) {
+            log.error("Get Review Reply List Is Null ! ! !");
+            throw new NullPointerException("리뷰 번호로 조회시 가져올 리뷰 리스트가 없습니디.");
+        } else {
+            log.info("Get Review Reply List By ReviewNo Is Success ! ! !");
+            return reviewReplyList;
+        }
     }
 }
