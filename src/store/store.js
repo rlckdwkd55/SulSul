d317e977b08eb2ce23eb01c0659d8e1574e6e920
dragonJ <yj9678joo@gmail.com>
2023-09-23 20:45:16 +0900

    리덕스

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './reducers/userSlice';
 
-export const store = configureStore({
+const store = configureStore({
     reducer: {
         user: userSlice,
     }
 })
+
+export default store;
